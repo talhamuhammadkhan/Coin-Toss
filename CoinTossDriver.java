d2dc0f9d2f920511e41abb84b8e1325e0acf3d7b
Talha Muhammad <muhammt5@ferris.edu>
2022-06-12 22:12:30 +0500

    Multiple coin toss simulation

--- CoinTossDriver.java
+++ CoinTossDriver.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class CoinTossDriver {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
+		int noOfFlips;
 		
+		System.out.println("Hi! Welcome to the coin toss simulator. Your coin will now be flipped. How many times do you want the coin to be flipped?");
 		
-		System.out.println("Hi! Welcome to the coin toss simulator. Your coin will now be flipped.");
+		noOfFlips = scnr.nextInt();
 		
-		Flip flip = new Flip();
-		flip.setResult();
-		System.out.println("The result is " + flip.getResult() + ".");   //Display result for this instance of the flip.
-	
+		for (int i = 1; i < noOfFlips + 1; ++i) {
+			Flip flip = new Flip();
+			flip.setResult();
+			System.out.println("The result of flip " + i + " is "+ flip.getResult() + ".");   //Display result for this instance of the flip.
+		}
 		
 	}
 }
