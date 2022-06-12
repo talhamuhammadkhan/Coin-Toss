a26f56e3e9b9556ea5215518e08d008e8ba10884
Talha Muhammad <muhammt5@ferris.edu>
2022-06-12 22:21:07 +0500

    Program now prints result statistics

--- CoinTossDriver.java
+++ CoinTossDriver.java
@@ -4,6 +4,8 @@ public class CoinTossDriver {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		int noOfFlips;
+		int headsCount = 0;
+		int tailsCount = 0;
 		
 		System.out.println("Hi! Welcome to the coin toss simulator. Your coin will now be flipped. How many times do you want the coin to be flipped?");
 		
@@ -13,7 +15,15 @@ public class CoinTossDriver {
 			Flip flip = new Flip();
 			flip.setResult();
 			System.out.println("The result of flip " + i + " is "+ flip.getResult() + ".");   //Display result for this instance of the flip.
+			if (flip.getResult() == "Heads") {
+				++headsCount;
+			}
+			else {
+				++tailsCount;
+			}
 		}
+		
+		System.out.println("\nYou flipped heads " + headsCount + " times and tails " + tailsCount + " times.");   //Display the number of times each result was flipped.
 		
 	}
 }
